ef693561c770a524c2660b4a8bc87be9e7c4b650
Gonzalo Torres <gonzalotorres317@gmail.com>
2025-09-30 18:17:49 -0300

    Footer corregido

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <>
             <footer className="bg-gray-800 text-white py-4">
                 <div className="container mx-auto text-center">
-                    <p className="text-sm">© 2025 Portafolio Martina Diaz. Todos los derechos reservados.</p>
+                    <p className="text-sm">© 2025 Portfolio Martina Diaz. Todos los derechos reservados.</p>
                 </div>
             </footer>
         </>
